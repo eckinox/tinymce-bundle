e5449eb7a5963167e126780dec529042cbf46531
Émile Perron <cunrakes@gmail.com>
2022-05-10 11:10:41 -0400

    fix: make formtype simple (not compound) This fixes form validation and data submission issues.

--- src/Form/Type/TinymceType.php
+++ src/Form/Type/TinymceType.php
@@ -18,6 +18,7 @@ class TinymceType extends AbstractType
 	public function configureOptions(OptionsResolver $resolver): void
 	{
 		$resolver->setDefaults([
+			'compound' => false,
 			'attr' => $this->tinyMceConfigurator->getGlobalAttributes()
 		]);
 	}
